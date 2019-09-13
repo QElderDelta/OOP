126e22ff1de8bf4644d69bc5f2f595f0a2d8dc21
QElderDelta <egorzhivalev@gmail.com>
2019-09-13 22:07:06 +0300

    added all source code and CMakeLists

---
+++ lab1/Modulo.hpp
@@ -0,0 +1,28 @@
+#ifndef _MODULO_H_
+#define _MODULO_H_
+
+
+#include <iostream>
+
+
+class Modulo {
+    public:
+        Modulo() : number(0), mod(0) {}
+        Modulo(int number, int mod) : number(number), mod(mod) {}
+        Modulo Add(const Modulo& addend) const;
+        Modulo Multiply(const Modulo& multiplier) const;
+        Modulo Subtract(const Modulo& subtracthend) const;
+        Modulo Divide(const Modulo& divisor) const;
+        void Read(std::istream& is);
+        void Print(std::ostream& os) const;
+        void SetNumber(int number);
+        void SetMod(int mod);
+        bool IsEqual(const Modulo& to_compare) const;
+        bool IsGreater(const Modulo& to_compare) const;
+        bool IsLess(const Modulo& to_compare) const;
+    private:
+        int number;
+        int mod;
+};
+
+#endif
